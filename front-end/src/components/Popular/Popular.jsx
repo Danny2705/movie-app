ea9892184f5bf3eb11e07b6ab58ff89b6b469904
Danny <nguyencaodat270504@gmail.com>
2024-04-13 02:53:27 -0600

    Fix route

--- front-end/src/components/Popular/Popular.jsx
+++ front-end/src/components/Popular/Popular.jsx
@@ -21,7 +21,6 @@ const genres = [
 ];
 
 export default function Popular() {
-
   const [selectedBtn, setSelectedBtn] = useState("All");
   const [movies, setMovies] = useState([]);
 
@@ -68,29 +67,32 @@ export default function Popular() {
           })}
         </div>
 
-				{/* A list of Movie Shows Here */}
-				<div className="flex flex-wrap w-full max-h-[1000px] no-scroll relative overflow-y-hidden justify-between gap-4 gap-y-8">
-					{movies.length === 0 ? (
-						<div>No movie data recorded</div>
-					) : (
-						movies.map(
-							(movie, i) =>
-								i < 15 && (
-									<div key={i} className="border max-w-[300px]">
-										<Card movie={movie} />
-									</div>
-								)
-						)
-					)}
-					<div className='absolute w-full h-[140px] fadeToBlack bottom-0 flex justify-center items-center '>
-						<div className='w-fit h-fit uppercase px-10 py-2 bg-main-red hover:bg-main-activate rounded-full mt-10 flex font-bold justify-center cursor-pointer items-center'>
-							Show all
-						</div>
-					</div>
-				</div>
-
-			</div>
-		</main>
-	);
-
+        {/* A list of Movie Shows Here */}
+        <div className='flex flex-wrap w-full max-h-[1000px] no-scroll relative overflow-y-hidden justify-between gap-4 gap-y-8'>
+          {movies.length === 0 ? (
+            <div>No movie data recorded</div>
+          ) : (
+            movies.map(
+              (movie, i) =>
+                i < 15 && (
+                  <Link
+                    key={i}
+                    to={`/library/title/${movie.title}/${movie.mal_id}`}
+                  >
+                    <div className='border max-w-[300px]'>
+                      {<Card movie={movie} />}
+                    </div>
+                  </Link>
+                )
+            )
+          )}
+          <div className='absolute w-full h-[140px] fadeToBlack bottom-0 flex justify-center items-center '>
+            <div className='w-fit h-fit uppercase px-10 py-2 bg-main-red hover:bg-main-activate rounded-full mt-10 flex font-bold justify-center cursor-pointer items-center'>
+              Show all
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
