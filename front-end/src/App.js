554f96bc631c9bb3b99415a88e8ddd5ee63739a5
James <112969081+HiusJames@users.noreply.github.com>
2024-04-15 02:01:23 -0600

    Add functionaloty for Search Bar and Retrieve Data

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import { Toaster } from "react-hot-toast";
 import Layout from "./screens/Layout/Layout";
 import MovieInfo from "./screens/MovieInfo/MovieInfo";
 import CharacterPage from "./screens/CharacterPage/CharacterPage";
+import SearchPage from "./screens/SearchPage/SearchPage";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
       />
       <Routes>
         <Route path='/' element={<Layout />} />
+        <Route path='/search' element={<SearchPage />} />
         <Route path='/library' element={<Navigate to='/library/ShowAll' />} />
         <Route path='/library/:letter' element={<LibraryPage />} />
         <Route path='/library/title/:title/:id' element={<MovieInfo />} />
--- front-end/src/components/Search/Search.jsx
+++ front-end/src/components/Search/Search.jsx
@@ -1,65 +1,84 @@
-import React, { useState } from "react";
-import { CiSearch } from "react-icons/ci";
-import searchData from "../../data/searchData.json";
-import { Link } from "react-router-dom";
-import { searchAnime } from "../../service.api.js/jikan.api";
+import React, { useState } from 'react';
+import { CiSearch } from 'react-icons/ci';
+import searchData from '../../data/searchData.json';
+import { Link, useNavigate } from 'react-router-dom';
+import { searchAnime } from '../../service.api.js/jikan.api';
 
 export default function Search() {
-  const [movieList, setMovieList] = useState([...searchData]);
-  const [showMovies, setShowMovies] = useState(false);
+	const [movieList, setMovieList] = useState([...searchData]);
+	const [showMovies, setShowMovies] = useState(false);
+  const [input, setInput] = useState('')
+  const navigate = useNavigate()
 
-  const search = async (prompt) => {
-    if (prompt === null) return;
-    const data = await searchAnime(prompt);
-    console.log(data.data);
-    setMovieList(data.data);
-  };
-  const handleInputBlur = () => {
-    // Delay the setShowMovies(false) to allow click event to be detected on dropdown items
-    setTimeout(() => {
-      setShowMovies(false);
-    }, 150);
-  };
-  return (
-    <div>
-      <div className='flex items-center border-2 border-[#e9e7e7] p-1 gap-1 cursor-pointer rounded-sm w-[250px]'>
-        <CiSearch onClick={() => setShowMovies(true)} />
-        <div className='h-[16px] border border-[#9298a3] w-px' />
-        <input
-          type='text'
-          placeholder='Search'
-          onFocus={() => setShowMovies(true)}
-          onBlur={handleInputBlur}
-          onKeyDown={(e) => {
-            e.key === "Enter" && search(e.target.value);
-          }}
-          className='outline-none pl-2 pt-1 bg-transparent text-sm text-[#ffffff] placeholder-white::placeholder font-josefin'
-        />
-      </div>
-      {showMovies === true && (
-        <div className='flex flex-col absolute z-10'>
-          {movieList.map(
-            (movie, index) =>
-              index < 5 && (
-                <div
-                  key={index}
-                  className='w-[250px] bg-opacity-80 bg-black px-3 flex flex-col pt-2 py-2 justify-center'
-                >
-                  <Link to={`/library/title/${movie.title}/${movie.mal_id}`}>
-                    <div className='flex items-center gap-4'>
-                      <img
-                        src={movie.images.webp.image_url}
-                        alt='Movie'
-                        className={`h-[100px]`}
-                      />
-                      <h1 className='w-full h-full text-sm'>{movie.title}</h1>
-                    </div>
-                  </Link>
-                </div>
-              )
-          )}
-        </div>
-      )}
-    </div>
-  );
+	const search = async (prompt) => {
+		if (prompt === null) return;
+		const data = await searchAnime(prompt);
+		console.log(data.data);
+		setMovieList(data.data);
+	};
+	const handleInputBlur = () => {
+		// Delay the setShowMovies(false) to allow click event to be detected on dropdown items
+		setTimeout(() => {
+			setShowMovies(false);
+		}, 150);
+	};
+
+  const handleBtnClick = async () => {
+    const prompt = document.querySelector('input[type="text"]').value;
+    if (prompt.trim() !== '') {
+      const data = await searchAnime(prompt);
+      navigate("/search", {state: {searchData: data.data}});
+    }
+  }
+  
+	return (
+		<div>
+			<div className="flex items-center border-2 border-[#e9e7e7] p-1 gap-1 cursor-pointer rounded-sm w-[250px]">
+				<CiSearch onClick={() => setShowMovies(true)} />
+				<div className="h-[16px] border border-[#9298a3] w-px" />
+				<input
+					type="text"
+					placeholder="Search"
+					onFocus={() => setShowMovies(true)}
+					onBlur={handleInputBlur}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+					onKeyDown={(e) => {
+						e.key === 'Enter' && search(e.target.value);
+					}}
+					className="outline-none pl-2 pt-1 bg-transparent text-sm text-[#ffffff] placeholder-white::placeholder font-josefin"
+				/>
+			</div>
+			{showMovies === true && (
+				<div className="flex flex-col absolute z-10">
+					{movieList.map(
+						(movie, index) =>
+							index < 5 && (
+								<div
+									key={index}
+									className="w-[250px] bg-opacity-80 bg-black px-3 flex flex-col pt-2 py-2 justify-center"
+								>
+									<Link to={`/library/title/${movie.title}/${movie.mal_id}`}>
+										<div className="flex items-center gap-4">
+											<img
+												src={movie.images.webp.image_url}
+												alt="Movie"
+												className={`h-[100px]`}
+											/>
+											<h1 className="w-full h-full text-sm">{movie.title}</h1>
+										</div>
+									</Link>
+								</div>
+							)
+					)}
+					{input ? <button
+						onClick={handleBtnClick}
+						className="flex w-full bg-main-red text-center items-center justify-center py-1"
+					>
+						Click to See More...
+					</button> : ''}
+				</div>
+			)}
+		</div>
+	);
 }
